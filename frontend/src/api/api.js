e8d81f3cc2980bcbb47b3e9704cadb5f9b5867a3
stellankilpatrick <stellankilpatrick@gmail.com>
2026-02-10 21:54:50 -0600

    Update API base URL to Render backend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000", // backend URL
+    baseURL: "https://album-rater-w3gl.onrender.com", // backend URL
 });
 
 api.interceptors.request.use((config) => {
