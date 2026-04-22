4a25164e60afff6b952b47c8f7b22ce3a0a2543f
Isak Mateen Pederson <isakmpederson@gmail.com>
2026-04-21 22:19:41 -0700

    Remove env/api changes, keep only auth page redesign

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
+    baseURL: "https://album-rater-w3gl.onrender.com", // backend URL
 });
 
 api.interceptors.request.use((config) => {
