e6d0947fe1d92058eb7711d0e4d3e5e70fc19b31
stellankilpatrick <stellankilpatrick@gmail.com>
2026-07-03 18:54:25 -0500

    timeago on comments

--- frontend/src/components/AlbumDetail.jsx
+++ frontend/src/components/AlbumDetail.jsx
@@ -61,7 +61,13 @@ export default function AlbumDetail({ user }) {
     const minutes = Math.floor(seconds / 60);
     const hours = Math.floor(minutes / 60);
     const days = Math.floor(hours / 24);
+    const weeks = Math.floor(days / 7);
+    const months = Math.floor(days / 30);
+    const years = Math.floor(days / 365);
 
+    if (years > 0) return `${years}y ago`;
+    if (months > 0) return `${months}mo ago`;
+    if (weeks > 0) return `${weeks}w ago`;
     if (days > 0) return `${days}d ago`;
     if (hours > 0) return `${hours}h ago`;
     if (minutes > 0) return `${minutes}m ago`;
