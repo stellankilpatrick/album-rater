83c0629b90926bd836a12395eb01fcf9e53855ff
stellankilpatrick <stellankilpatrick@gmail.com>
2026-09-15 18:57:45 -0500

    fixed

--- backend/models/album.models.js
+++ backend/models/album.models.js
@@ -456,7 +456,7 @@ export async function getAlbumDetailsPrivate(albumId, userId) {
     JOIN artists ar ON ar.id = aa.artist_id
     LEFT JOIN album_ratings alr ON alr.album_id = a.id AND alr.user_id = $1
     WHERE a.id = $2
-    GROUP BY a.id, alr.rating, alr.rated_songs, alr.review, alr.id, alr.untracked`,
+    GROUP BY a.id, alr.rating, alr.rated_songs, alr.review, alr.id, alr.untracked, alr.adjustor`,
     [userId, albumId]
   );
 
